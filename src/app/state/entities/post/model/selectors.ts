a748bf6a76052a404a17e38381f4a4ec1568ecf1
immortal-p <immortalkil4oy@gmail.com>
2026-02-05 21:56:45 +0300

    feat(change): fix name

--- src/app/state/entities/post/model/selectors.ts
+++ src/app/state/entities/post/model/selectors.ts
@@ -1,13 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
-import type { RootStore } from '../../..';
+import type { RootState } from '../../../index';
 import { postsAdapter } from './slice';
 
-export const { selectAll: selectAllPosts } = postsAdapter.getSelectors<RootStore>(
+export const { selectAll: selectAllPosts } = postsAdapter.getSelectors<RootState>(
     (state) => state.posts,
 );
 
 export const selectorPostsByFeedId = createSelector(
-    [selectAllPosts, (_state: RootStore, feedId: string | null) => feedId],
+    [selectAllPosts, (_state: RootState, feedId: string | null) => feedId],
     (posts, feedId) => {
         return posts.filter((post) => post.feedId === feedId);
     },
